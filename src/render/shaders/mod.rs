9b2d0df49c3da5d9731c1ef876fcfcc8cfe2498d
erwanvivien <erwanvivien@Erwans-MacBook-Pro.local>
2023-08-16 15:19:39 +0900

    feat: shader can switch between color & texture
    We use a technique called uber shader, where the shader
    does many things[1]
    This has the drawbaw of having conditionnal code in shader
    but allows to have only one pipeline, it should be benched
    at some point.
    
    [1] https://psgraphics.blogspot.com/2021/12/what-is-uber-shader.html

--- src/render/shaders/mod.rs
+++ src/render/shaders/mod.rs
@@ -2,13 +2,15 @@ use std::{collections::HashMap, rc::Rc};
 
 use crate::utils::load_file_string;
 
+pub mod kind;
+
 static mut GLOBAL_SHADERS: Option<HashMap<String, Rc<wgpu::ShaderModule>>> = None;
 
-const SHADERS: [&str; 2] = ["albedo_shader", "texture_shader"];
+const SHADERS: [&str; 1] = ["main_shader"];
 
 pub async fn build_shaders(device: &wgpu::Device) {
     for shader_name in &SHADERS {
-        get_shader_or_build(shader_name, device).await;
+        build_shader(device, shader_name).await;
     }
 }
 
@@ -16,10 +18,10 @@ pub fn get_shader(name: &str) -> Rc<wgpu::ShaderModule> {
     #[cfg(feature = "debug_shader")]
     log::info!("Getting shader {:?}", name);
 
-    unsafe { GLOBAL_SHADERS.as_ref() }.expect("Shaders not built")[name].clone()
+    unsafe { GLOBAL_SHADERS.as_ref() }.expect("No shaders built")[name].clone()
 }
 
-async fn get_shader_or_build(name: &str, device: &wgpu::Device) -> Rc<wgpu::ShaderModule> {
+async fn build_shader(device: &wgpu::Device, name: &str) {
     if unsafe { GLOBAL_SHADERS.as_ref() }.is_none() {
         #[cfg(feature = "debug_shader")]
         log::info!("Init shader storage");
@@ -29,14 +31,14 @@ async fn get_shader_or_build(name: &str, device: &wgpu::Device) -> Rc<wgpu::Shad
     let global_shaders = unsafe { GLOBAL_SHADERS.as_mut() };
     if global_shaders.as_ref().unwrap().contains_key(name) {
         #[cfg(feature = "debug_shader")]
-        log::info!("Using cached shader {:?}", name);
-        return unsafe { GLOBAL_SHADERS.as_ref() }.unwrap()[name].clone();
+        log::info!("Shader already built: {}", name);
+        return;
     }
 
     #[cfg(feature = "debug_shader")]
     log::info!("Building shader {:?}", name);
 
-    let shader = load_file_string(format!("assets/{name}.wgsl"))
+    let shader: String = load_file_string(format!("assets/{name}.wgsl"))
         .await
         .expect("Could not read shader");
 
@@ -51,6 +53,4 @@ async fn get_shader_or_build(name: &str, device: &wgpu::Device) -> Rc<wgpu::Shad
     global_shaders
         .unwrap()
         .insert(String::from(name), shader.clone());
-
-    shader
 }
